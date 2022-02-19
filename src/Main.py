8452c06f62644ae195e39cfb50ec98e7451a7b92
Davids Paics <temporardavids@gmail.com>
2022-02-19 20:13:46 +0200

    space out code

--- src/Main.py
+++ src/Main.py
@@ -16,10 +16,14 @@ if verbose:
 
 pygame.init()
 win = pygame.display.set_mode((1080, 720))
+
 # variables
+
 run = True
 clock = pygame.time.Clock()
+
 # mainloop
+
 while run:
     clock.tick(60)
     for event in pygame.event.get():
