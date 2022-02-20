36962d3dd9bc7cc9edaa6be38080e7699784cd67
Davids Paics <temporardavids@gmail.com>
2022-02-20 12:49:50 +0200

    commit on main

--- src/Main.py
+++ src/Main.py
@@ -31,6 +31,9 @@ background = pygame.transform.scale(background, (1080, 720))
 pygame.mouse.set_visible(False)
 pygame.display.set_caption("Geometry shoot")
 
+
+currentframe = 0
+
 # mainloop
 while run:
     # update background
@@ -38,17 +41,23 @@ while run:
 
     # create ground (13 tiles long (see cell size in utils/globalInfo.py))
     for i in range(13):
-        objectUtil.drawObject(screen, i, 0, 1, verbose=True)
+       objectUtil.drawObject(screen, i, 0, 1, verbose=True)
 
     objectUtil.drawObject(screen, 4, 1, 3, verbose=True)
     objectUtil.drawObject(screen, 5, 1, 3, rotation="flipped", verbose=True)
     objectUtil.drawObject(screen, 7, 1, 3, verbose=True, rotation="down")
     objectUtil.drawObject(screen, 8, 1, 3, rotation="flipped down", verbose=True)
 
+    if currentframe % 2 == 0:
+        objectUtil.drawObject(screen, 6, 3, 5, verbose=True)
+    else:
+        objectUtil.drawObject(screen, 5, 3, 5, verbose=True)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
     pygame.display.flip()
+    currentframe += 1
     clock.tick(60)
 pygame.quit()
