d82e3052256fae6627a04731a40f412952849a52
Davids Paics <temporardavids@gmail.com>
2022-02-20 13:16:53 +0200

    update globalInfo

--- src/utils/globalInfo.py
+++ src/utils/globalInfo.py
@@ -1,7 +1,9 @@
 currentframe = 0
 
+
 class variables(object):
     def __init__(self):
         self.gridCellSize = 85
         self.worldHeightCells = 20
         self.worldHeightPx = self.worldHeightCells * self.gridCellSize
+        self.sideScrollSpeed = 5
